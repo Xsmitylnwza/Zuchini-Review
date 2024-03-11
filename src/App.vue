925724ad65a0bde857754ab565e94555d3d19bf4
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2024-03-11 21:37:33 +0700

    add new db.json

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 import MoviePage from './components/Homepage/MoviePage.vue';
 import NavBar from './components/Homepage/NavBar.vue';
 import Homepage from './components/Homepage/Homepage.vue';
-import data from '/data/db.json'
+
+const movies = ref([]);
 
 
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost:5000/movies');
+    const data = await response.json();
+    movies.value = data.slice(0, 5);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 </script>
 
@@ -13,17 +24,11 @@ import data from '/data/db.json'
     <template v-slot:navBar>
       <NavBar />
     </template>
-
     <template v-slot:moviePage>
-      <MoviePage />
-      <MoviePage />
-      <MoviePage />
-
+      <MoviePage v-for="movie in movies" :movie="movie" :key="movie.id" />
     </template>
   </Homepage>
-  <div class="w-[100vh] h-[100vh]"></div>
-
-
+  <div class="w-[100vh] h-[100vh] "></div>
 </template>
 
-<style scoped></style>./components/Homepage/MoviePage.vue./components/Homepage/NavBar.vue
+<style scoped></style>
--- src/components/Homepage/Homepage.vue
+++ src/components/Homepage/Homepage.vue
@@ -1,13 +1,12 @@
 <script setup>
-
-
-
 </script>
 
 <template>
-    <div class="laptop:bg-cover min-h-[100vh] max-w-[100%]" :style="{ 'background-image': 'url(/image/avenger.jpg)' }">
+    <div class="laptop:bg-cover h-[100vh] max-w-[100%]"
+        :style="{ 'background-image': 'url(' + '/image/avenger.jpg' + ')' }">
+
         <slot name="navBar"></slot>
-        <div class="flex justify-center gap-[150px] overflow-hidden">
+        <div class="flex justify-center gap-[150px]">
             <slot name="moviePage"></slot>
         </div>
     </div>
