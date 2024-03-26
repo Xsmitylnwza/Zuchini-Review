cc474f5b4cf5e292134efad75bc07c6a6ab8269e
damn <110712863+saksaran@users.noreply.github.com>
2024-03-27 00:08:34 +0700

    add review, commented routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,35 +4,46 @@ import RegisterView from '@/views/RegisterView.vue'
 import LoginView from '@/views/LoginView.vue'
 import MovieDes from '@/components/descriptionpage/MovieDescriptionPage.vue'
 import NotFound from '@/views/NotFound.vue'
+import ReviewPage from '@/components/reviewpage/ReviewPage.vue'
+import CommentedPage from '@/components/commentedpage/CommentedPage.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Homepage
-        },
-        {
-            path: '/register',
-            name: 'register',
-            component: RegisterView
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: LoginView
-        },
-        {
-            path: '/movie/:id',
-            component: MovieDes,
-        },
-        {
-            path: '/:notfound(.*)',
-            component: NotFound
-        }
-    ]
-
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Homepage,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/movie/:id',
+      component: MovieDes,
+    },
+    {
+      path: '/:notfound(.*)',
+      component: NotFound,
+    },
+    {
+      name: 'review',
+      path: '/review/:id',
+      component: ReviewPage,
+    },
+    {
+      path: '/commented',
+      name: 'commented',
+      component: CommentedPage,
+    },
+  ],
 })
 
 export default router
